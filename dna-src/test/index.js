21ddf6711f56ce1592879cb0c5f60982a85b7e9a
AmrTarek <amrtarekmo@gmail.com>
2019-03-12 23:53:26 +0200

    Upgrade holochain to 0.0.6 alpha

--- dna-src/test/index.js
+++ dna-src/test/index.js
@@ -3,7 +3,7 @@
 const { Config, Scenario } = require("@holochain/holochain-nodejs")
 Scenario.setTape(require("tape"))
 
-const dnaPath = "./dist/bundle.json"
+const dnaPath = "./dist/dna-src.dna.json"
 const agentAlice = Config.agent("alice")
 const dna = Config.dna(dnaPath)
 const instanceAlice = Config.instance(agentAlice, dna)
@@ -12,9 +12,11 @@ const scenario = new Scenario([instanceAlice])
 scenario.runTape("description of example test", (t, { alice }) => {
   // Make a call to a Zome function
   // indicating the function, and passing it an input
-  const addr = alice.call("my_zome", "create_my_entry", {"entry" : {"content":"sample content"}})
-  const result = alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
-  
+  const addr = alice.call("holoster", "register", {"name":"amr","avatar_url":"sample url"})
+  console.log(addr);
+
+  //const result = alice.call("my_zome", "get_my_entry", {"address": addr.Ok})
+
   // check for equality of the actual and expected results
-  t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
+  //t.deepEqual(result, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
 })
