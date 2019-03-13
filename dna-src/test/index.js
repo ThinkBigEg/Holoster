3cfab7b40b31d2a455ef67c8e5b011d3a4c918cf
AmrTarek <amrtarekmo@gmail.com>
2019-03-13 22:47:58 +0200

    Tested 'get_member_profile'

--- dna-src/test/index.js
+++ dna-src/test/index.js
@@ -7,13 +7,11 @@ const instanceAlice = Config.instance(agentAlice, dna)
 const scenario = new Scenario([instanceAlice])
 
 
-scenario.runTape('Can register a profile and retrieve', async (t, {alice}) => {
-  const register_result = await alice.callSync('holoster', 'register', {name: 'alice', avatar_url: ''})
-  console.log(register_result)
-  //t.true(register_result.Ok.includes('alice'))
+scenario.runTape('Register 2 profiles and retrieve them', async (t, {alice}) => {
+  const register_address1 = await alice.callSync('holoster', 'register', {name: 'alice1', avatar_url: ''})
+  const register_address2 = await alice.callSync('holoster', 'register', {name: 'alice2', avatar_url: ''})
+  console.log(register_address1)
 
-
-  const get_profile_result = await alice.callSync('holoster', 'get_member_profile', {agent_address: register_result.Ok})
+  const get_profile_result = await alice.callSync('holoster', 'get_member_profile', {agent_address: register_address1.Ok})
   console.log(get_profile_result)
-
 })
