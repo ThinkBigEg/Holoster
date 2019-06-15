e88505c892c6e6b5727d1d7543ba15a5329e4ece
abdelazizElemary <abdelaziz528@gmail.com>
2019-06-15 17:24:58 +0200

    added sidebar and timeline

--- ui-src/src/app/app.module.ts
+++ ui-src/src/app/app.module.ts
@@ -1,24 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { SignupComponent } from "./signup/signup.component";
-
-import { ReactiveFormsModule } from "@angular/forms";
-import { PostsComponent } from "./posts/posts.component";
-import { UsersComponent } from "./users/users.component";
-import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, SignupComponent, PostsComponent, UsersComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
