c16dbe636b92710dc7c6d0a204d980b7a54de56f
abdelrahmanqabil <abdelrahmanqabil@gmail.com>
2019-06-21 19:00:00 +0200

    project run problem fixing

--- ui-src/src/app/app.module.ts
+++ ui-src/src/app/app.module.ts
@@ -1,27 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { PostPageComponent } from './post-page/post-page.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { PostPageComponent } from "./post-page/post-page.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { ReactiveFormsModule } from "@angular/forms";
 import { UserProfileComponent } from "./UserProfile/UserProfile.component";
-import { UsersComponent } from "./users/users.component";
 
 import { HttpClientModule } from "@angular/common/http";
 import { PostUpdateComponent } from "./post-update/post-update.component";
-import {SignupComponent} from "./signup/signup.component";
+import { SignupComponent } from "./signup/signup.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     UserProfileComponent,
-    UsersComponent,
+    SidebarComponent,
+    PostPageComponent,
     PostUpdateComponent
-
   ],
   imports: [
     BrowserModule,
@@ -32,4 +31,4 @@ import {SignupComponent} from "./signup/signup.component";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- ui-src/src/karma.conf.js
+++ ui-src/src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/ui-src'),
+      dir: require('path').join(__dirname, '../coverage/holoster'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
