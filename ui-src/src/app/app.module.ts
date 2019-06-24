88284dbbb23f83c29fe48b364e0b46408a8805e6
abdelazizElemary <abdelaziz528@gmail.com>
2019-06-24 17:24:16 +0200

    followers and following components added

--- ui-src/src/app/app.module.ts
+++ ui-src/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TimelineComponent } from "./timeline/timeline.component";
 import { SignupComponent } from "./signup/signup.component";
 import { UserProfileComponent } from "./user-profile/user-profile.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FollowersListComponent } from './followers-list/followers-list.component';
+import { UnfollowersListComponent } from './unfollowers-list/unfollowers-list.component';
+import { FollowingListComponent } from './following-list/following-list.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,10 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     PostPageComponent,
     SignupComponent,
     UserProfileComponent,
-    MyProfileComponent
+    MyProfileComponent,
+    FollowersListComponent,
+    UnfollowersListComponent,
+    FollowingListComponent
   ],
   imports: [
     BrowserModule,
---
+++ ui-src/src/app/following-list/following-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-following-list',
+  templateUrl: './following-list.component.html',
+  styleUrls: ['./following-list.component.css']
+})
+export class FollowingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
